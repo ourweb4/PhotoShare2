a97999b13e0009b09d1b8bdf71db844c429250ce
Bill Banks <ourweb1@bills-imac.local>
2016-12-21 14:41:20 -0500

    Initial Commit

---
+++ PhotoShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoShare
+//
+//  Created by Bill Banks on 12/21/16.
+//  Copyright © 2016 Bill Banks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
